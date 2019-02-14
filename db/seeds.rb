db800bfc2b4329ac856b3c79c9e7dee1f080dc74
roni talvi <vtalvi@gmail.com>
2019-02-14 14:14:20 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 require 'json'
-require 'byebug'
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 cocktails_serialized = open(url).read
 cocktails = JSON.parse(cocktails_serialized)
